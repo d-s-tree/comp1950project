5ba0b5b10ea56dd1148199507820248ea44aa33b
mwerner12 <mwerner1226@gmail.com>
2018-03-26 21:39:09 -0700

    Meta Style Changes
    updated the stylesheet for the metapage.

---
+++ js/scrollup.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            document.getElementById("scrollBtn").style.display = "block";
+        } else {
+            document.getElementById("scrollBtn").style.display = "none";
+        }
+    }
+
+    document.getElementById("scrollBtn").onclick = function() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+}
